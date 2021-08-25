f1d12884f80cb556472a8109e822c25fdff3c077
Ryan Mehta <ryan.mehta@gmail.com>
2021-08-25 08:08:32 -0700

    update starter

--- packages/cra-template-near/template/src/App.jsx
+++ packages/cra-template-near/template/src/App.jsx
@@ -1,19 +1,68 @@
-import React, { useEffect } from "react";
-import { NearSignin } from "near-react-hooks";
+import React, { useEffect, useState } from "react";
+import { NearSignin, useNearContract, useNearWallet } from "near-react-hooks";
 
 export default function App() {
-  const near = useNear();
   const wallet = useNearWallet();
-  const contract = useNearContract("dev-123457824879", {
-    viewMethods: ["getCount"],
-    changeMethods: ["decrement", "increment"],
+  const contract = useNearContract("counter.react.testnet", {
+    changeMethods: ["decrement", "increment", "reset"],
+    viewMethods: ["get_num"],
   });
 
+  const [loading, setLoading] = useState(false);
+  const [balance, setBalance] = useState(0);
+
+  const getBalance = async () => {
+    const balance = await contract.get_num();
+    setBalance(balance);
+  };
+
+  const decrement = async () => {
+    setLoading(true);
+
+    await contract.decrement();
+    await getBalance();
+
+    setLoading(false);
+  };
+
+  const increment = async () => {
+    setLoading(true);
+
+    await contract.increment();
+    await getBalance();
+
+    setLoading(false);
+  };
+
+  const reset = async () => {
+    setLoading(true);
+
+    await contract.reset();
+    await getBalance();
+
+    setLoading(false);
+  };
+
   useEffect(() => {
-    if (!wallet.isSignedIn()) wallet.requestSignIn();
+    getBalance();
   }, []);
 
-  if (!wallet.isSignedIn()) return null;
+  return (
+    <section style={{ textAlign: "center" }}>
+      <h1>Thanks for using CRA-Template-NEAR!</h1>
+      <NearSignin
+        contractId="counter.react.testnet"
+        methodNames={["decrement", "increment", "reset"]}
+      />
+      <p>Current Number: {loading ? "loading..." : balance}</p>
 
-  return <NearSignin contractId="wrap.near" />;
+      {!wallet.isSignedIn() ? null : (
+        <>
+          <button onClick={decrement}>Decrement</button>
+          <button onClick={increment}>Increment</button>
+          <button onClick={reset}>Reset</button>
+        </>
+      )}
+    </section>
+  );
 }
